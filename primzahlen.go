38b39dd94459e8dc41922cd6e4e17b66c8f3601c
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-26 16:40:30 +0100

    Hinweise entfernt.

--- primzahlen.go
+++ primzahlen.go
@@ -19,25 +19,13 @@ func Divides(m, n int) bool {
 // Erwartet eine Zahl n.
 // Liefert true, falls n eine Primzahl ist.
 func IsPrime(n int) bool {
-	// Hinweis: Prüfen Sie mit einer Schleife für jede der Zahlen i von 2 bis n-1,
-	// ob n durch i teilbar ist. Wenn Sie einen Teiler finden, können Sie sofort
-	// (noch innerhalb der Schleife) false liefern.
-	// Läuft die Schleife durch, ohne einen Teiler zu finden, ist n eine Primzahl.
-	// Die Schleife ist als Gerüst vorgegeben, die Prüfung für en vorzeitigen Abbruch
-	// müssen Sie noch einbauen.
-	// Außerdem müssen Sie vor der Schleife evtl. noch Sonderfälle betrachten.
-	for i := 2; i < n; i++ {
-		// TODO: Prüfen, ob n durch i teilbar ist und false liefern, falls ja.
-	}
+	// TODO
 	return true
 }
 
 // Erwartet eine Zahl n.
 // Gibt alle Primzahlen auf der Konsole aus, die kleiner als n sind.
 func PrintPrimes(n int) {
-	// Hinweis: Laufen Sie in einer Schleife über alle Zahlen von 2 bis n und geben
-	// Sie sie aus, falls es Primzahlen sind.
-	// Sie können das Schleifengerüst aus IsPrime() kopieren.
 	// TODO
 }
 
@@ -45,11 +33,8 @@ func PrintPrimes(n int) {
 // Liefert die nächstgrößere Primzahl.
 // Liefert n, falls n selbst eine Primzahl ist.
 func NextPrime(n int) int {
-	// Hinweis: Laufen Sie mit einer Schleife ab n und zählen Sie den Schleifenzähler
-	// hoch, bis Sie eine Primzahl gefunden haben.
-	result := n
-	// TODO: result hochzählen, bis es eine Primzahl ist.
-	return result
+	// TODO
+	return 0
 }
 
 // Erwartet eine Zahl n.
@@ -59,18 +44,14 @@ func NextPrime(n int) int {
 // * k ist eine Primzahl
 // * k + 2 ist eine Primzahl
 func NextPrimeTwin(n int) int {
-	// Hinweis: Schreiben Sie eine Schleife, die bei n = NextPrime(n) beginnt und n
-	// so lange hochzählt, bis sowohl n als auch n+2 Primzahlen sind.
 	// TODO
-	return n
+	return 0
 }
 
 // Erwartet eine Zahl n.
 // Liefert die größte Primzahl, die echt kleiner als n ist.
 // Falls es keine solche Zahl gibt, wird 0 geliefert.
 func GreatestPrimeBelow(n int) int {
-	// Hinweis: Schreiben Sie eine Schleife, die n so lange herunterzählt,
-	// bis es eine Primzahl ist.
 	// TODO
-	return n
+	return 0
 }
